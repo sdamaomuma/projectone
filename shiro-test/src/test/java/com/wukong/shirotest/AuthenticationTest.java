98afec89d7250aca9250235a1511d89296a17f27
2388697919@qq.com <2388697919@qq.com>
2020-03-16 13:50:25 +0800

    Initial commit

---
+++ shiro-test/src/test/java/com/wukong/shirotest/AuthenticationTest.java
@@ -0,0 +1,16 @@
+package com.wukong.shirotest;
+
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.junit.Test;
+
+public class AuthenticationTest {
+
+    @Test
+    public  void  testAuthentication(){
+
+        //1、构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+
+
+    }
+}
